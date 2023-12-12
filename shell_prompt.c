4b1b2797acd7a6e27db61712425333296aa3f35f
Collatera24 <kola.tella24@gmail.com>
2023-12-11 20:58:15 -0800

    Function that displays prompt

--- shell_prompt.c
+++ shell_prompt.c
@@ -1,5 +1,13 @@
 #include "shell.h"
 
+/**
+ * display_prompt - Function that displays the customised prompt
+ * on the terminal
+ * custom_printf - Customised printf function
+ *
+ * Return: 0
+ */
+
 void display_prompt(void)
 {
 	custom_printf("kola && hammed_shell# ");
