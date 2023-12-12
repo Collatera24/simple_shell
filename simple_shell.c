ff37dd810ac63f7b7a0a12bca71369737b9a4b40
Collatera24 <kola.tella24@gmail.com>
2023-12-11 20:46:31 -0800

    Main entry point

---
+++ simple_shell.c
@@ -0,0 +1,20 @@
+#include "simple_shell.h"
+/**
+ * main - Entry point
+ * command: A character array to store the user input command,
+ * assumes a maximum command length of 104 characters.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char command[105];
+	
+	while (1)
+	{
+		display_prompt();
+		read_command(command, sizeof(command));
+		execute_command(command);
+	}
+	return (0);
+}
