01f7848246d84d7b027febef5b0abedfd059089e
Collatera24 <kola.tella24@gmail.com>
2023-12-13 09:28:28 -0800

    Shell tokenisation file

--- client_input.c
+++ client_input.c
@@ -26,3 +26,52 @@ void read_command(char *command, size_t size)
 	}
 	command[strcspn(command, "\n")] = '\0'; /* Remove new line */
 }
+
+/**
+ * execute_command - Reads, interprets and tokenise commands
+ * @command: Strings to be tokenised
+ *
+ * Return: 0
+ * Author - Kolawole Tella and Hammed Yakub
+ */
+
+void execute_command(const char *command)
+{
+	pid_t child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		custom_printf("Error message.\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid == 0)
+	{
+		/**
+		 * Child process
+		 * Parse the command and its arguments
+		 */
+		char *args[105]; /* Maximum 105 arguments */
+		int arg_count = 0;
+
+		char *token = strtok((char *)command, " ");
+
+		while (token != NULL)
+		{
+			args[arg_count++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[arg_count] = NULL;
+
+		/* Execute the command */
+		execve(args[0], args, NULL);
+
+		/* If execve fails, return an error message */
+		custom_printf("Error message.\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		/* Parent proccess */
+		wait(NULL);
+	}
+}
